cadb18a6df1d2118a71b6c74f3d5d2eff43af494
Cláudio Tomás <ccaducarlos@gmail.com>
2023-03-02 16:50:28 +0200

    finishe sidebar population

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Tweet } from "./components/Tweet";
-import "./global.css"
-import twitterLogo from "./assets/Logo.svg"
+import "./global.css";
+import twitterLogo from "./assets/Logo.svg";
+import { Bell, BookmarkSimple, DotsThreeCircle, EnvelopeSimple, FileText, Hash, House, User } from "phosphor-react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<div className="layout">
 			<aside className="sidebar">
 				<img className="logo" src={twitterLogo} alt="logo" />
-				<nav></nav>
-				<button className="new-tweet" type="button">Tweet</button>
+				<nav className="main-navigation">
+					<a href="" className="active">
+						<House size={32} weight="fill" /> Home
+					</a>
+					<a href="">
+						<Hash size={32} color="#0d0c0c" />
+						Explore
+					</a>
+					<a href="">
+						<Bell size={32} color="#0d0c0c" /> Notifications
+					</a>
+					<a href="">
+						<EnvelopeSimple size={32} color="#0d0c0c" /> Messages
+					</a>
+					<a href="">
+						<BookmarkSimple size={32} color="#0d0c0c" /> Bookmarks
+					</a>
+					<a href="">
+						<FileText size={32} color="#0d0c0c" />
+						Lists
+					</a>
+					<a href="">
+						<User size={32} color="#0d0c0c" />
+						Profile
+					</a>
+					<a href="">
+						<DotsThreeCircle size={32} color="#0d0c0c" />
+						More
+					</a>
+				</nav>
+				<button className="new-tweet" type="button">
+					Tweet
+				</button>
 			</aside>
 			<div className="content">content</div>
 		</div>
