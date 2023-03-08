b1b4e019fc868f7403b69101b5456bb42edda02f
Cláudio Tomás <ccaducarlos@gmail.com>
2023-03-08 08:54:15 +0200

    c

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import "./styles/global.css";
 import { Sidebar } from "./components/Sidebar";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./components/routes";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<Router>
+			<RouterProvider router={router} />
+		</Router>
 	</React.StrictMode>
 );
